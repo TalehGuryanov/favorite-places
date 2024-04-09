541bca5b6d16884d2fedb8731a284153ec9eff88
Taleh Guryanov <taleh.gurjanov@gmail.com>
2024-04-09 21:16:46 +0300

    added navigation

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import {AllPlaces} from "./screens/AllPlaces";
+import {AddPlace} from "./screens/AddPlace";
+import {GlobalStyles} from "./constants/styles";
+import {IconButton} from "./components/ui/IconButton";
+import {PlaceDetails} from "./screens/PlaceDetails";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {backgroundColor: GlobalStyles.colors.primary500},
+            headerTintColor: GlobalStyles.colors.gray700,
+            contentStyle: {backgroundColor: GlobalStyles.colors.white}
+          }}
+        >
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({navigation}) => ({
+              title: 'Your Favorite Places',
+              headerRight: ({tintColor}) => {
+                return (
+                  <IconButton
+                      name="add"
+                      size={24}
+                      color={tintColor}
+                      onPress={() => navigation.navigate('AddPlace')}
+                  />
+                );
+              }
+            })}
+          />
+          <Stack.Screen
+              name="AddPlace"
+              component={AddPlace}
+              option={{
+                title: 'Add a new Place',
+              }}
+          />
+          <Stack.Screen
+              option={{
+                title: 'Place Details',
+              }}
+              name="PlaceDetails"
+              component={PlaceDetails}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
